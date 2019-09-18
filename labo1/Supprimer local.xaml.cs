1592ca482b4f344620189a7d2145130fe177def8
Claudia <loranger.claudia@gmail.com>
2019-09-18 09:17:41 -0400

    First commit

---
+++ labo1/Supprimer local.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace labo1
+{
+    /// <summary>
+    /// Logique d'interaction pour Supprimer_local.xaml
+    /// </summary>
+    public partial class Supprimer_local : Window
+    {
+        public Supprimer_local()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
